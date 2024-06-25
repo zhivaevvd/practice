6af85587f7f27139ede664a32bd51715c9b55fab
Живаев Владислав Дмитриевич <57016265+zhivaevvd@users.noreply.github.com>
2024-06-25 17:36:05 +0300

    Profile improvements (#4)
    Co-authored-by: Vlad Zhivaev <vlad.zhivaev45@gmail.com>

--- Front/Practice/Practice/Classes/Business/Models/Profile.swift
+++ Front/Practice/Practice/Classes/Business/Models/Profile.swift
@@ -1,11 +1,39 @@
 
 import Foundation
 
-struct Profile: Decodable, Hashable, Equatable, Encodable {
+struct ProfileType: Codable, RawRepresentable, Hashable, Equatable {
+    init(rawValue: String) {
+        self.rawValue = rawValue
+    }
+    
+    static let admin: ProfileType = .init(rawValue: "admin")
+    static let student: ProfileType = .init(rawValue: "student")
+    static let teacher: ProfileType = .init(rawValue: "teacher")
+    
+    let rawValue: String
+}
+
+struct Profile: Codable, Hashable, Equatable {
     let id: Int
     let authId: Int
     let name: String
     let surname: String
-    let occupation: String
+    let type: ProfileType
     let avatarUrl: String?
+    let groupId: Int?
+}
+
+extension Profile {
+    var readableType: String {
+        switch type {
+        case .admin:
+            return "Администратор"
+        case .student:
+            return "Студент"
+        case .teacher:
+            return "Преподаватель"
+        default:
+            return ""
+        }
+    }
 }
--- Front/Practice/Practice/Classes/Modules/TabBar/TabBarVC.swift
+++ Front/Practice/Practice/Classes/Modules/TabBar/TabBarVC.swift
@@ -2,5 +2,14 @@
 import UIKit
 
 class TabBarVC: UITabBarController {
-    override func viewDidLoad() {}
+    override func viewDidLoad() {
+        guard dataService?.appState.user?.type == .student, var controllers = self.viewControllers else {
+            return
+        }
+        
+        controllers.remove(at: 1)
+        self.viewControllers = controllers
+    }
+    
+    var dataService: DataService?
 }
